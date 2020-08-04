1703afcdc0e209786db4042632cf54ee7ccbafa0
Shannon Trainor <sltrainor@outlook.com>
2020-08-04 15:50:18 -0400

    added cache event listeners & responses

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,59 @@
 //cache while offline
 
+let CACHE_NAME = "budget-tracker-cache";
+const DATA_CACHE_NAME = "data-cache";
+
+let urlsToCache = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+//install
+self.addEventListener("install", (event) => {
+    event.waitUntil (
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log("Cache Open");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+
+//cache requests to api routes
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("/api")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request).then(response => {
+                    //if response 200, clone
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone())
+                    }
+                    return response;
+                })
+                //this will catch an error to network and pull from cache instead
+                .catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+
+    //go to main html page in cache
+    event.respondWith(fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+            if (response) {
+                return response;
+            } else if (event.request.headers.get("accept").includes("text.html")) {
+                return caches.match("/");
+            }
+        });
+        })
+    );
+});
