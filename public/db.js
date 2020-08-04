369858683718f6a68f2c4c667beb9469cbb88163
Shannon Trainor <sltrainor@outlook.com>
2020-08-04 16:05:12 -0400

    added checkout & save functions

--- public/db.js
+++ public/db.js
@@ -4,3 +4,45 @@ window.mozIndexedDB ||
 window.webkitIndexedDB ||
 window.msIndexedDB ||
 window.shimIndexedDB;
+
+let db;
+const request = indexedDB.open("budget", 1);
+
+
+
+
+saveRecord((record) => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+})
+
+
+checkDatabase(() => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, /", "Content-Type": "application/json"
+                }
+            }).then(response => {
+                return response.json();
+            }).then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
+})
+
+
+//listen for online connection
+window.addEventListener("online", checkDatabase);
