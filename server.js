3d59180b16b4d1eb4308ac8887078cb528d7538d
Shannon Trainor <sltrainor@outlook.com>
2020-08-11 19:37:11 -0400

    changed /budget

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
