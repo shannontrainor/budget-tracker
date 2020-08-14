2e8d3b319b7767ac6ac48019dde9d69843ae1817
Shannon Trainor <sltrainor@outlook.com>
2020-08-13 21:46:19 -0400

    added mongo_uri for heroku deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
